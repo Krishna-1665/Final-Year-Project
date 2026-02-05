0d44bc8f2c5252e42c1c82ae2befd395c71ae281
krishna <bhardwajkrishna166@gmail.com>
2026-02-05 15:28:10 +0530

    venv cleared

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,11 @@ from flask import Flask, request, jsonify
 import joblib, os
 from utils.preprocessing import clean_text  # if you have it
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # optional: load model if present
-MODEL_PATH = os.path.join(os.path.dirname(_file_), 'model', 'interview_model.joblib')
-VEC_PATH = os.path.join(os.path.dirname(_file_), 'model', 'vectorizer.joblib')
+MODEL_PATH = os.path.join(os.path.dirname(__file__), 'model', 'interview_model.joblib')
+VEC_PATH = os.path.join(os.path.dirname(__file__), 'model', 'vectorizer.joblib')
 MODEL = joblib.load(MODEL_PATH) if os.path.exists(MODEL_PATH) else None
 VEC = joblib.load(VEC_PATH) if os.path.exists(VEC_PATH) else None
 
@@ -17,10 +17,7 @@ def index():
 @app.route('/health')
 def health():
     return jsonify({"status": "ok"}), 200
-
-
-
-if _name_ == '_main_':
+if __name__ == '__main__':
     for rule in app.url_map.iter_rules():
         print(rule, list(rule.methods))
     app.run(debug=True)
